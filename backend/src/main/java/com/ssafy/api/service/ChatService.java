695f27b72b64a1e51adb1e791049e3b66ec9e08d
Jin Heung An <wlsgmd1535@naver.com>
2021-07-22 10:45:46 +0900

    BE-chat

---
+++ backend/src/main/java/com/ssafy/api/service/ChatService.java
@@ -0,0 +1,42 @@
+package com.ssafy.api.service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.api.dto.ChatRoom;
+
+
+@Repository
+public class ChatService {
+    private Map<String, ChatRoom> chatRoomMap;
+
+    @PostConstruct
+    private void init() {
+        chatRoomMap = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        // 채팅방 생성순서 최근 순으로 반환
+        List chatRooms = new ArrayList<>(chatRoomMap.values());
+        Collections.reverse(chatRooms);
+        return chatRooms;
+    }
+
+    public ChatRoom findRoomById(String id) {
+        return chatRoomMap.get(id);
+    }
+
+    public ChatRoom createChatRoom(String name) {
+        ChatRoom chatRoom = ChatRoom.create(name);
+        System.out.println(chatRoom.getRoomId());
+        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
+    }
+}
