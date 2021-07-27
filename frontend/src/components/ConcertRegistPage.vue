3604d8ad9520dacb122b71fbf04dc8e6b7193067
강민주 <alswn8972@daum.net>
2021-07-27 13:44:03 +0900

    Modify concertRegist

--- frontend/src/components/ConcertRegistPage.vue
+++ frontend/src/components/ConcertRegistPage.vue
@@ -9,91 +9,55 @@
             <div class="row clearfix">
               <div class="form-group col-lg-12 col-md-12 col-sm-12">
                 <div class="field-inner">
-                  <input
-                    type="text"
-                    v-model="concert.userid"
-                    name="UserId"
-                    value=""
-                    placeholder="Id"
-                    required=""
-                  />
+                  <input type="text" v-model="concert.title" name="Title" value="" placeholder="Title"  required=""/>
                 </div>
               </div>
               <div class="form-group col-lg-12 col-md-12 col-sm-12">
                 <div class="field-inner">
-                  <input
-                    type="text"
-                    v-model="concert.title"
-                    name="Title"
-                    value=""
-                    placeholder="Title"
-                    required=""
-                  />
-                </div>
-              </div>
-              <div class="form-group col-lg-12 col-md-12 col-sm-12">
-                <div class="field-inner">
-                  <input
-                    type="text"
-                    v-model="concert.description"
-                    name="Description"
-                    value=""
-                    placeholder="Description"
-                    required=""
-                  />
+                  <input type="text" v-model="concert.description" name="Description" value="" placeholder="Description" required=""/>
                 </div>
               </div>
               <div class="form-group col-lg-6 col-md-12 col-sm-12">
                 <div class="field-inner">
-                  <input
-                    type="text"
-                    v-model="concert.startTime"
-                    name="StartTime"
-                    value=""
-                    placeholder="StartTime"
-                    required=""
-                  />
+                  <input type="text" v-model="concert.startTime" name="StartTime" value="" placeholder="StartTime" required=""/>
                 </div>
               </div>
               <div class="form-group col-lg-6 col-md-12 col-sm-12">
                 <div class="field-inner">
-                  <input
-                    type="text"
-                    v-model="concert.endTime"
-                    name="EndTime"
-                    value=""
-                    placeholder="EndTime"
-                    required=""
-                  />
+                  <input type="text" v-model="concert.endTime" name="EndTime" value="" placeholder="EndTime" required=""/>
                 </div>
               </div>
               <div class="form-group col-lg-6 col-md-12 col-sm-12">
                 <div class="field-inner">
-                  <input
-                    type="text"
-                    v-model="concert.priceVip"
-                    name="PriceVip"
-                    value=""
-                    placeholder="PriceVip"
-                    required=""
-                  />
+                  <input type="text" v-model="concert.priceVip" name="PriceVip" value="" placeholder="PriceVip" required=""/>
                 </div>
               </div>
               <div class="form-group col-lg-6 col-md-12 col-sm-12">
                 <div class="field-inner">
-                  <input
-                    type="file"
-                    name="files"
-                    value=""
-                    placeholder="file"
-                    required=""
-                  />
+                  <input type="text" v-model="concert.priceStand" name="PriceStand" value="" placeholder="PriceStand" required=""/>
+                </div>
+              </div>
+              <div class="form-group col-lg-6 col-md-12 col-sm-12">
+                <div class="field-inner">
+                  <select v-model="concert.categoryName" name="CategoryId" value="" placeholder="CategoryId" required="">
+                    <!-- 이부분 수정 필요 -->
+                    <option value="">공연 카테고리</option>
+                    <option value="HIPHOP">HipHop</option>
+							      <option value="발라드">Balad</option>
+                    <option value="재즈">Zazz</option>
+                    <option value="인디">Indi</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group col-lg-6 col-md-12 col-sm-12">
+                <div class="field-inner">
+                  <input type="file" ref="picture" name="files" required="">
                 </div>
               </div>
               <div class="form-group col-lg-12 col-md-12 col-sm-12">
                 <button class="theme-btn btn-style-one">
                   <i class="btn-curve"></i>
-                  <span @click="clickLogin" class="btn-title">Login</span>
+                  <span @click="clickRegist" class="btn-title">콘서트 신청</span>
                 </button>
               </div>
             </div>
@@ -110,13 +74,14 @@ export default {
   data:function(){
     return{
       concert: {
-        userid: "",
         title: "",
         description:"",
         startTime:"",
         endTime:"",
         priceVip:"",
         priceStand:"",
+        categoryName:"",
+        files: null,
       },
       message: ""
     }
@@ -127,9 +92,10 @@ export default {
   },
 
   methods:{
-    ...mapActions('user',["requestLogin"]),
-    clickLogin:function(){
-      this.requestLogin(this.user);
+    ...mapActions('concert',["requestRegister"]),
+    clickRegist:function(){
+      this.concert.files = this.$refs.picture.files[0];
+      this.requestRegister(this.concert);
     }
   }
 };
--- frontend/src/store/modules/concert.js
+++ frontend/src/store/modules/concert.js
@@ -1,4 +1,5 @@
-// import http from '@/util/http-common';
+import http from "@/util/http-common";
+//import router from "@/router/index.js";
 export default {
     namespaced: true,
     state: {
@@ -7,19 +8,43 @@ export default {
             {category:"Ballad"},
             {category:"Zazz"},
             {category:"Indi"}
-        ]
+        ],
+        concertList:[],
     },
     getters: {
         getConcertCategory(state) {
             return state.concertCategory;
         },
-        
+        getConcertList(state) {
+            return state.concertList;
+        }
     },
     mutations: {
         
   },
     actions: {
-        
+        requestRegister(commit, concert) {
+            var formData = new FormData();
+            const CSRF_TOKEN=localStorage.getItem("accessToken");
+            for (var variable in concert) {
+              formData.append(variable, concert[variable]);
+              console.log(variable, concert[variable]);
+            }
+            
+            http
+              .post(`/api/v2/concert/regist`, formData, {
+                headers: {  "Authorization": 'Bearer '+ CSRF_TOKEN, 
+                            "Content-Type": "multipart/form-data" 
+                },
+              })
+              .then(() => {
+                alert('공연 신청이 완료되었습니다.')
+              })
+              .catch((err) => {
+                alert(err.response.data.message);
+                  console.error();
+              });
+          },
     },
   };
   
