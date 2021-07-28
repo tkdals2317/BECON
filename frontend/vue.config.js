7516fa3fe2ca6a5aa9f48f387f2dc3bdeee444dc
Jin Heung An <wlsgmd1535@naver.com>
2021-07-28 23:38:16 +0900

    ADD Jar file

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,9 +6,18 @@ module.exports = {
       port: 8083,
       open: true,
       proxy: {
-        '/api': {
+        '/api/v1': {
           target: 'http://localhost:8080/'
-        }
+        },
+        '/api/v2': {
+          target: 'http://localhost:8080/'
+        },
+        '/groupcall': {
+          target: 'http://localhost:8080/'
+        },
+        '/upload': {
+          target: 'http://localhost:8080/'
+        },
       },
       historyApiFallback: true,
       hot: true
@@ -16,9 +25,6 @@ module.exports = {
     css: {
       requireModuleExtension: false // import 시에 경로에 .module 포함 안해도 됨.
     },
-    transpileDependencies: [
-      'element-plus'
-    ],
     lintOnSave: false,
     outputDir: '../backend/src/main/resources/dist',
   }
