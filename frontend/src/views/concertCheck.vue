7c25e0fb9db0c83c36ed789639e68c5575b5691f
강민주 <alswn8972@daum.net>
2021-07-29 21:32:09 +0900

    Add RegistConcertList

---
+++ frontend/src/views/concertCheck.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Nav />
+    <PageHeader title="Concert Check" />
+    <ConcertCheck />
+    <Footer />
+  </div>
+</template>
+<script>
+import Nav from "../components/Nav";
+import PageHeader from "../components/PageHeader";
+import Footer from "../components/Footer";
+import ConcertCheck from "../components/ConcertCheck";
+export default {
+  components: {
+    ConcertCheck,
+    Footer,
+    PageHeader,
+    Nav,
+  },
+  head() {
+    return {
+      title: "Be-con | My Services | Check Concert",
+    };
+  },
+};
+</script>
---
+++ frontend/src/views/service.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Nav />
+    <PageHeader title="My Services" />
+    <ServicesPage />
+    <Footer />
+  </div>
+</template>
+<script>
+  import Nav from "../components/Nav";
+  import PageHeader from "../components/PageHeader";
+  import Footer from "../components/Footer";
+  import ServicesPage from "../components/ServicesPage";
+    export default {
+    components: {
+      ServicesPage,
+      Footer,
+      PageHeader,
+      Nav
+    },
+    head(){
+      return {
+        title: "BeCone | My Services"
+      }
+    }
+  }
+</script>
