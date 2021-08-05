12d51fe7c9f8df22c50b9b0312ba71db5cd72651
이상민 <tkdals2317@naver.com>
2021-08-05 11:47:31 +0900

    Modfiy baseURL

--- frontend/src/common/lib/http.js
+++ frontend/src/common/lib/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://3.36.67.58:8080',
+    baseURL:'https://3.36.67.58:8443',
     //baseURL:'http://localhost:8080',
     headers:{
         'Content-type':'application/json'
